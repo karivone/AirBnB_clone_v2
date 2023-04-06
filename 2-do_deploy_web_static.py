ab9b0eba58073c04655f0b343c177dee5132362e
kayewi <55494837+karivone@users.noreply.github.com>
2023-04-06 14:16:57 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,27 +3,59 @@
 archive to the web servers
 
 
-from fabric.api import put, run, env
+from fabric.api import *
+from os import path
 from os.path import exists
+from datetime import datetime
 env.hosts = ['18.235.255.209', '34.207.189.167']
+  
+env.user = "ubuntu" 
 
-
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-        return False
+env.key_filename = '~/.ssh/id_rsa' 
+  
+  
+def do_deploy(archive_path): 
+     """ 
+     Deploys an archive to the specified remote servers 
+     through ssh 
+  
+     Attr: 
+         archive_path (str): The path of our compressed archive 
+     """ 
+     try: 
+         if not (path.exists(archive_path)): 
+             return False 
+  
+         # Upload the the archive to the /tmp directory of the server 
+         put(archive_path, '/tmp/') 
+  
+         # Create the destination folder 
+         time_stamp = archive_path[-18:-4] 
+         run('mkdir -p /data/web_static/releases/web_static_{}/'.format 
+             (time_stamp)) 
+  
+         # Uncompress the archive to the web server 
+         run("tar -xzf /tmp/web_static_{}.tgz -C \ 
+             /data/web_static/releases/web_static_{}/".format 
+             (time_stamp, time_stamp)) 
+  
+         # Delete the archive from the web server 
+         run('rm /tmp/web_static_{}.tgz'.format(time_stamp)) 
+  
+         # Delete the symbolic link from the web server 
+         run("mv /data/web_static/releases/web_static_{}/web_static/* \ 
+             /data/web_static/releases/web_static_{}/".format 
+             (time_stamp, time_stamp)) 
+         run("rm -rf /data/web_static/releases/web_static_{}/web_static" 
+             .format(time_stamp)) 
+         run("rm -rf /data/web_static/current") 
+  
+         # Create a new symbolic link 
+         run("ln -s /data/web_static/releases/web_static_{}/ \ 
+             /data/web_static/current".format(time_stamp)) 
+  
+         print("New Version deployed!") 
+     except Exception as e: 
+         return False 
+  
+     return True
